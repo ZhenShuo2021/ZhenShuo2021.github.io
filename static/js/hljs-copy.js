69f470b8e27e096fc276099537a7fc5f32c1695e
ZhenShuo2021 <98386542+ZhenShuo2021@users.noreply.github.com>
2024-08-15 15:59:43 +0800

    Fix hljs-copy

--- static/js/hljs-copy.js
+++ static/js/hljs-copy.js
@@ -1,17 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  document.querySelectorAll('pre code').forEach((codeBlock) => {
-    // Create a button element
-    const button = document.createElement('button');
-    button.className = 'copy-btn';
-    button.type = 'button';
-    button.innerText = 'Copy';
-    
-    // Attach the button to the code block
-    codeBlock.parentNode.appendChild(button);
+  const container = document.querySelector('body'); // 根據需要選擇更具體的容器
 
-    // Add click event listener
-    button.addEventListener('click', () => {
+  container.addEventListener('click', (event) => {
+    if (event.target.classList.contains('copy-btn')) {
+      const button = event.target;
+      const codeBlock = button.previousElementSibling;
       const text = codeBlock.innerText;
+
       navigator.clipboard.writeText(text).then(() => {
         button.innerText = 'Copied!';
         setTimeout(() => {
@@ -20,10 +15,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
       }).catch(err => {
         console.error('Failed to copy text: ', err);
       });
-    });
+    }
+  });
+
+  document.querySelectorAll('pre code').forEach((codeBlock) => {
+    const button = document.createElement('button');
+    button.className = 'copy-btn';
+    button.type = 'button';
+    button.innerText = 'Copy';
+    
+    codeBlock.parentNode.appendChild(button);
   });
 
   document.querySelectorAll('pre code').forEach((block) => {
-    hljs.highlightBlock(block);
+    hljs.highlightElement(block);
   });
 });
